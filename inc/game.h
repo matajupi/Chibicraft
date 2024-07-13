89a1d11ba6a713c4f96cda0c800b4bd82cf96eb4
Kosuke.F <kousukehutamata@gmail.com>
2024-07-14 04:46:10 +0900

    ブロックの配置、破壊を実装 :)

--- inc/game.h
+++ inc/game.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstdint>
 #include <cassert>
+#include <string>
+#include <iostream>
 #include <glm/glm.hpp>
 
 class Game {
@@ -12,21 +14,15 @@ public:
     void Start();
 
 private:
-    static constexpr const int kNTexs = 8;
-
-    static constexpr const int kTexWidth = 64;
-    static constexpr const int kTexHeight = 64;
-
+    // ======== Map ========
     static constexpr const int kMapWidth = 26;
     static constexpr const int kMapDepth = 26;
     static constexpr const int kMapHeight = 8;
 
-    static constexpr const int kMaxRayDist = 20;
-
-    static constexpr const float kPlayerHalfWidth = 0.3;
-    static constexpr const float kPlayerHalfDepth = 0.3;
-    static constexpr const float kPlayerUpperHalfHeight = 0.3;
-    static constexpr const float kPlayerLowerHalfHeight = 1.3;
+    // AirBlock, TransparentBlockもBlockとして含む
+    static constexpr const int kNBlocks = 10;
+    static constexpr const int kAirBlock = 0;
+    static constexpr const int kTransparentBlock = kNBlocks - 1;
 
     char world_map_[kMapHeight * kMapWidth * kMapDepth];
 
@@ -35,27 +31,73 @@ private:
             0 <= z && z < kMapDepth);
         return world_map_[y * kMapWidth * kMapDepth + x * kMapDepth + z];
     }
+    char GetMapBlock(const glm::ivec3 &map_pos) const {
+        return GetMapBlock(map_pos.x, map_pos.y, map_pos.z);
+    }
+    void SetMapBlock(int x, int y, int z, char block) {
+        assert(0 <= x && x < kMapWidth && 0 <= y && y < kMapHeight &&
+            0 <= z && z < kMapDepth && 0 <= block && block < kNBlocks);
+        world_map_[y * kMapWidth * kMapDepth + x * kMapDepth + z] = block;
+    }
+    void SetMapBlock(const glm::ivec3 &map_pos, char block) {
+        SetMapBlock(map_pos.x, map_pos.y, map_pos.z, block);
+    }
+    void LoadMap(int mid);
 
-    int screen_width_;
-    int screen_height_;
-    bool fullscreen_;
+    // ======== Textures ========
+    static constexpr const int kNTexs = 3;
 
-    std::vector<std::vector<uint32_t>> texs_;
-    // static constexpr const int kTransparentBlock = 255;
-    static constexpr const int kTransparentBlock = 9;
+    static constexpr const int kTexWidth = 16;
+    static constexpr const int kTexHeight = 16;
 
-    uint32_t GetTexColor(int tex, int x, int y) {
+    static const std::string kTexDir;
+    static const std::array<std::string, kNTexs> kTexFiles;
+    static const std::array<long long int, kNBlocks> kBlockToTexs;
+    std::vector<uint32_t> texs_[kNTexs];
+
+    // face: ブロックの面
+    // 0: x-面, 1: x+面, 2: y-面, 3: y+面, 4: z-面, 5: z+面
+    int GetTex(int block, int face) const {
+        assert(0 <= block && block < kNBlocks && 0 <= face && face < 6);
+        return kBlockToTexs[block] >> (face * 8) & 0xff;
+    }
+    uint32_t GetTexColor(int tex, int x, int y) const {
         assert(0 <= tex && tex < kNTexs && 0 <= x && x < kTexWidth &&
             0 <= y && y < kTexHeight);
         return texs_[tex][kTexWidth * y + x];
     }
+    void LoadTexs();
+
+    // ======== Ray ========
+    static constexpr const int kMaxRayDist = 20;
+
+    struct Ray {
+        glm::vec3 dir;
+        glm::ivec3 pos;
+        // collision_side: 衝突した面に垂直な軸
+        // 0: x軸(面はy-z平面), 1: y軸, 2: z軸
+        int collision_side;
+        float perp_wall_dist;
+        float max_perp_wall_dist;
+    };
+
+    // ======== Player ========
+    static constexpr const float kPlayerHalfWidth = 0.3;
+    static constexpr const float kPlayerHalfDepth = 0.3;
+    static constexpr const float kPlayerUpperHalfHeight = 0.3;
+    static constexpr const float kPlayerLowerHalfHeight = 1.3;
+
+    static constexpr const float kPlayerPutBlockDist = 3.5;
+    static constexpr const float kPlayerDestBlockDist = 3.5;
 
     glm::vec3 pos_, dir_, plane_x_, plane_y_;
 
-    float time_;
-    float old_time_;
-    float frame_time_;
+    // ======== Screen ========
+    int screen_width_;
+    int screen_height_;
+    bool fullscreen_;
 
+    // Stack overflowを起こすので、Heap領域にメモリを確保
     uint32_t *buffer_;
 
     uint32_t GetBufColor(int x, int y) const {
@@ -67,21 +109,39 @@ private:
         buffer_[screen_width_ * y + x] = color;
     }
 
-    void LoadMap(int mid);
-    void LoadTexs();
+    // ======== Time ========
+    float time_;
+    float old_time_;
+    float frame_time_;
+
+    // ======== Input ========
+    bool prev_lmb_;
+    bool prev_rmb_;
 
     void Init();
     void InitScreen();
     void InitRaycaster();
 
     void Update();
+    bool CastRay(int x, int y, Ray &ray) const;
+    uint32_t CalcPixelColor(const Ray &ray) const;
     void Raycasting();
 
-    bool CanRotateY(float angle);
-    bool CanMoveX(float mvx);
-    bool CanMoveY(float mvy);
-    bool CanMoveZ(float mvz);
+    // part: Playerの部位を指定
+    // x: 0: x-(Playerのx-面のx座標), 1: x+
+    // y: 0: y-, 1: y(Playerの中心のy座標), 2: y+
+    // z: 0: z-, 1: z+
+    glm::ivec3 GetPlayerPartPos(const glm::ivec3 &part) const;
+    bool HitBlock(const std::vector<glm::ivec3> &parts) const;
+    void TryRotateY(float angle);
+    void TryMoveX(float mvx);
+    void TryMoveY(float mvy);
+    void TryMoveZ(float mvz);
 
+    void HandleKeys();
+    void HandleMouseMove(int mouse_x, int mouse_y);
+    void OnLeftButtonPress();
+    void OnRightButtonPress();
     void HandleInput();
 
     void Quit();
--- src/game.cc
+++ src/game.cc
@@ -25,13 +25,33 @@ namespace {
     }
 }
 
-Game::Game(int screen_width, int screen_height, bool fullscreen) {
+const std::string Game::kTexDir = "bedrock-samples/resource_pack/textures/blocks/";
+const std::array<std::string, Game::kNTexs> Game::kTexFiles = {
+    "grass_carried.png",            // 0
+    "grass_side_carried.png",       // 1
+    "dirt.png",                     // 2
+};
+// 6面を6byteで指定
+const std::array<long long int, Game::kNBlocks> Game::kBlockToTexs = {
+    0xffffffffffff,                 // Air
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0x010100020101,                 // Grass
+    0xffffffffffff,                 // Transparent
+};
+
+Game::Game(int screen_width, int screen_height, bool fullscreen)
+    : fullscreen_(fullscreen), prev_lmb_(false) {
     if (screen_width < 0 || screen_height < 0) {
         GetScreenResolution(screen_width, screen_height);
     }
     screen_width_ = screen_width;
     screen_height_ = screen_height;
-    fullscreen_ = fullscreen;
 }
 
 void Game::Start() {
@@ -75,20 +95,12 @@ void Game::LoadMap(int mid) {
 }
 
 void Game::LoadTexs() {
-    texs_.resize(kNTexs);
-    for (int i = 0; kNTexs > i; i++)
-        texs_[i].resize(kTexWidth * kTexHeight);
-
     unsigned long tw, th, err = 0;
-    err |= QuickCG::loadImage(texs_[0], tw, th, "res/texs/eagle.png");
-    err |= QuickCG::loadImage(texs_[1], tw, th, "res/texs/redbrick.png");
-err |= QuickCG::loadImage(texs_[3], tw, th, "res/texs/purplestone.png");
-    err |= QuickCG::loadImage(texs_[2], tw, th, "res/texs/greystone.png");
-    err |= QuickCG::loadImage(texs_[4], tw, th, "res/texs/bluestone.png");
-err |= QuickCG::loadImage(texs_[6], tw, th, "res/texs/mossy.png");
-    err |= QuickCG::loadImage(texs_[5], tw, th, "res/texs/wood.png");
-    err |= QuickCG::loadImage(texs_[7], tw, th, "res/texs/colorstone.png");
-    if (err) {
+    for (int i = 0; kNTexs > i; i++) {
+        texs_[i].resize(kTexWidth * kTexHeight);
+        err |= QuickCG::loadImage(texs_[i], tw, th, kTexDir + kTexFiles[i]);
+    }
+   if (err) {
         std::cerr << "Error: Failed to load textures." << std::endl;
         Quit();
     }
@@ -114,135 +126,162 @@ void Game::Update() {
     QuickCG::redraw();
 }
 
+bool Game::CastRay(int x, int y, Ray &ray) const {
+    float camera_y = 2.0 * y / screen_height_ - 1;
+    float camera_x = 2.0 * x / screen_width_ - 1;
+
+    ray.dir = dir_ + plane_x_ * camera_x + plane_y_ * camera_y;
+    ray.pos = pos_;
+
+    float delta_dist_x = (ray.dir.x == 0) ? 1e30 : std::abs(1 / ray.dir.x);
+    float delta_dist_y = (ray.dir.y == 0) ? 1e30 : std::abs(1 / ray.dir.y);
+    float delta_dist_z = (ray.dir.z == 0) ? 1e30 : std::abs(1 / ray.dir.z);
+
+    int step_x, step_y, step_z;
+    float side_dist_x, side_dist_y, side_dist_z;
+    if (ray.dir.x < 0) {
+        step_x = -1;
+        side_dist_x = (pos_.x - ray.pos.x) * delta_dist_x;
+    }
+    else {
+        step_x = 1;
+        side_dist_x = (ray.pos.x + 1.0 - pos_.x) * delta_dist_x;
+    }
+    if (ray.dir.y < 0) {
+        step_y = -1;
+        side_dist_y = (pos_.y - ray.pos.y) * delta_dist_y;
+    }
+    else {
+        step_y = 1;
+        side_dist_y = (ray.pos.y + 1.0 - pos_.y) * delta_dist_y;
+    }
+    if (ray.dir.z < 0) {
+        step_z = -1;
+        side_dist_z = (pos_.z - ray.pos.z) * delta_dist_z;
+    }
+    else {
+        step_z = 1;
+        side_dist_z = (ray.pos.z + 1.0 - pos_.z) * delta_dist_z;
+    }
+
+    bool hit = false;
+    while (!hit) {
+        if (side_dist_x <= side_dist_y
+         && side_dist_x <= side_dist_z
+         && side_dist_x <= kMaxRayDist) {
+            side_dist_x += delta_dist_x;
+            ray.pos.x += step_x;
+            ray.collision_side = 0;
+        }
+        else if (side_dist_y <= side_dist_z
+              && side_dist_y <= kMaxRayDist) {
+            side_dist_y += delta_dist_y;
+            ray.pos.y += step_y;
+            ray.collision_side = 1;
+        }
+        else if (side_dist_z <= kMaxRayDist) {
+            side_dist_z += delta_dist_z;
+            ray.pos.z += step_z;
+            ray.collision_side = 2;
+        }
+        else {
+            break;
+        }
+        if (ray.pos.x < 0 || ray.pos.x >= kMapWidth ||
+            ray.pos.y < 0 || ray.pos.y >= kMapHeight ||
+            ray.pos.z < 0 || ray.pos.z >= kMapDepth) {
+            break;
+        }
+        int block = GetMapBlock(ray.pos);
+        hit = block != kAirBlock && block != kTransparentBlock;
+    }
+
+    if (ray.collision_side == 0) {
+        ray.perp_wall_dist = (side_dist_x - delta_dist_x);
+        ray.max_perp_wall_dist = (kMaxRayDist - delta_dist_x);
+    }
+    else if (ray.collision_side == 1) {
+        ray.perp_wall_dist = (side_dist_y - delta_dist_y);
+        ray.max_perp_wall_dist = (kMaxRayDist - delta_dist_y);
+    }
+    else {
+        ray.perp_wall_dist = (side_dist_z - delta_dist_z);
+        ray.max_perp_wall_dist = (kMaxRayDist - delta_dist_z);
+    }
+
+    return hit;
+}
+
+uint32_t Game::CalcPixelColor(const Ray &ray) const {
+    char block = GetMapBlock(ray.pos);
+    float wall_x, wall_y;
+    if (ray.collision_side == 0) {
+        wall_x = pos_.z + ray.perp_wall_dist * ray.dir.z;
+        wall_y = pos_.y + ray.perp_wall_dist * ray.dir.y;
+    }
+    else if (ray.collision_side == 1) {
+        wall_x = pos_.x + ray.perp_wall_dist * ray.dir.x;
+        wall_y = pos_.z + ray.perp_wall_dist * ray.dir.z;
+    }
+    else {
+        wall_x = pos_.x + ray.perp_wall_dist * ray.dir.x;
+        wall_y = pos_.y + ray.perp_wall_dist * ray.dir.y;
+    }
+    wall_x -= floor(wall_x);
+    wall_y -= floor(wall_y);
+
+    int tex_x = wall_x * kTexWidth;
+    int tex_y = wall_y * kTexHeight;
+    int face;
+    if (ray.collision_side == 0) {
+        if (ray.dir.x > 0) {
+            tex_x = kTexWidth - tex_x - 1;
+            face = 0;
+        }
+        else {
+            face = 1;
+        }
+    }
+    if (ray.collision_side == 1) {
+        if (ray.dir.y > 0) {
+            tex_x = kTexWidth - tex_x - 1;
+            face = 2;
+        }
+        else {
+            face = 3;
+        }
+    }
+    if (ray.collision_side == 2) {
+        if (ray.dir.z < 0) {
+            tex_x = kTexWidth - tex_x - 1;
+            face = 4;
+        }
+        else {
+            face = 5;
+        }
+    }
+
+    int tex = GetTex(block, face);
+    uint32_t color = GetTexColor(tex, tex_x, kTexHeight - tex_y - 1);
+    if (ray.collision_side == 1 || ray.collision_side == 2) {
+        color = (color >> 1) & 0x7F7F7F;
+    }
+    uint32_t fog = std::min(0xFFu,
+        (uint32_t)(0xFF * ray.perp_wall_dist / ray.max_perp_wall_dist));
+    color =
+        std::min((color >> 16 & 0xFF) + fog, 0xFFu) << 16 |
+        std::min((color >> 8  & 0xFF) + fog, 0xFFu) << 8  |
+        std::min((color       & 0xFF) + fog, 0xFFu);
+    return color;
+}
+
 void Game::Raycasting() {
     for (int y = 0; screen_height_ > y; y++) {
-        float camera_y = 2.0 * y / screen_height_ - 1;
         for (int x = 0; screen_width_ > x; x++) {
-            float camera_x = 2.0 * x / screen_width_ - 1;
-            glm::vec3 raydir = dir_ + plane_x_ * camera_x + plane_y_ * camera_y;
-
-            int map_x = pos_.x;
-            int map_y = pos_.y;
-            int map_z = pos_.z;
-
-            float delta_dist_x = (raydir.x == 0) ? 1e30 : std::abs(1 / raydir.x);
-            float delta_dist_y = (raydir.y == 0) ? 1e30 : std::abs(1 / raydir.y);
-            float delta_dist_z = (raydir.z == 0) ? 1e30 : std::abs(1 / raydir.z);
-
-            int step_x, step_y, step_z;
-            float side_dist_x, side_dist_y, side_dist_z;
-            if (raydir.x < 0) {
-                step_x = -1;
-                side_dist_x = (pos_.x - map_x) * delta_dist_x;
-            }
-            else {
-                step_x = 1;
-                side_dist_x = (map_x + 1.0 - pos_.x) * delta_dist_x;
-            }
-            if (raydir.y < 0) {
-                step_y = -1;
-                side_dist_y = (pos_.y - map_y) * delta_dist_y;
-            }
-            else {
-                step_y = 1;
-                side_dist_y = (map_y + 1.0 - pos_.y) * delta_dist_y;
-            }
-            if (raydir.z < 0) {
-                step_z = -1;
-                side_dist_z = (pos_.z - map_z) * delta_dist_z;
-            }
-            else {
-                step_z = 1;
-                side_dist_z = (map_z + 1.0 - pos_.z) * delta_dist_z;
-            }
-
-            int side;
-            bool hit = false;
-            while (!hit) {
-                if (side_dist_x <= side_dist_y
-                 && side_dist_x <= side_dist_z
-                 && side_dist_x <= kMaxRayDist) {
-                    side_dist_x += delta_dist_x;
-                    map_x += step_x;
-                    side = 0;
-                }
-                else if (side_dist_y <= side_dist_z
-                      && side_dist_y <= kMaxRayDist) {
-                    side_dist_y += delta_dist_y;
-                    map_y += step_y;
-                    side = 1;
-                }
-                else if (side_dist_z <= kMaxRayDist) {
-                    side_dist_z += delta_dist_z;
-                    map_z += step_z;
-                    side = 2;
-                }
-                else {
-                    break;
-                }
-                if (map_x < 0 || map_x >= kMapWidth ||
-                    map_y < 0 || map_y >= kMapHeight ||
-                    map_z < 0 || map_z >= kMapDepth) {
-                    break;
-                }
-                int tex = GetMapBlock(map_x, map_y, map_z);
-                hit = tex > 0 && tex != kTransparentBlock;
-            }
+            Ray ray;
+            bool hit = CastRay(x, y, ray);
             if (hit) {
-                float perp_wall_dist;
-                float max_perp_wall_dist;
-                if (side == 0) {
-                    perp_wall_dist = (side_dist_x - delta_dist_x);
-                    max_perp_wall_dist = (kMaxRayDist - delta_dist_x);
-                }
-                else if (side == 1) {
-                    perp_wall_dist = (side_dist_y - delta_dist_y);
-                    max_perp_wall_dist = (kMaxRayDist - delta_dist_y);
-                }
-                else {
-                    perp_wall_dist = (side_dist_z - delta_dist_z);
-                    max_perp_wall_dist = (kMaxRayDist - delta_dist_z);
-                }
-
-                int tex = GetMapBlock(map_x, map_y, map_z) - 1;
-                float wall_x, wall_y;
-                if (side == 0) {
-                    wall_x = pos_.z + perp_wall_dist * raydir.z;
-                    wall_y = pos_.y + perp_wall_dist * raydir.y;
-                }
-                else if (side == 1) {
-                    wall_x = pos_.x + perp_wall_dist * raydir.x;
-                    wall_y = pos_.z + perp_wall_dist * raydir.z;
-                }
-                else {
-                    wall_x = pos_.x + perp_wall_dist * raydir.x;
-                    wall_y = pos_.y + perp_wall_dist * raydir.y;
-                }
-                wall_x -= floor(wall_x);
-                wall_y -= floor(wall_y);
-
-                int tex_x = wall_x * kTexWidth;
-                int tex_y = wall_y * kTexHeight;
-                if (side == 0 && raydir.x > 0) {
-                    tex_x = kTexWidth - tex_x - 1;
-                }
-                if (side == 1 && raydir.y > 0) {
-                    tex_x = kTexWidth - tex_x - 1;
-                }
-                if (side == 2 && raydir.z < 0) {
-                    tex_x = kTexWidth - tex_x - 1;
-                }
-
-                uint32_t color = GetTexColor(tex, tex_x, kTexHeight - tex_y - 1);
-                if (side == 1 || side == 2) {
-                    color = (color >> 1) & 0x7F7F7F;
-                }
-                uint32_t fog = std::min(0xFFu,
-                    (uint32_t)(0xFF * perp_wall_dist / max_perp_wall_dist));
-                color =
-                    std::min((color >> 16 & 0xFF) + fog, 0xFFu) << 16 |
-                    std::min((color >> 8  & 0xFF) + fog, 0xFFu) << 8  |
-                    std::min((color       & 0xFF) + fog, 0xFFu);
+                uint32_t color = CalcPixelColor(ray);
                 SetBufColor(x, screen_height_ - y - 1, color);
             }
             else {
@@ -252,9 +291,8 @@ void Game::Raycasting() {
     }
 }
 
-void Game::HandleInput() {
+void Game::HandleKeys() {
     float move_speed = frame_time_ * 2.0;
-    float rot_speed = frame_time_ * 0.05;
 
     glm::vec3 perdir = glm::normalize(plane_x_);
     glm::vec3 mvdir(0, 0, 0);
@@ -283,19 +321,15 @@ void Game::HandleInput() {
     }
     if (glm::length(mvdir) > 0) {
         mvdir = move_speed * glm::normalize(mvdir);
-        if (CanMoveX(mvdir.x)) {
-            pos_.x += mvdir.x;
-        }
-        if (CanMoveY(mvdir.y)) {
-            pos_.y += mvdir.y;
-        }
-        if (CanMoveZ(mvdir.z)) {
-            pos_.z += mvdir.z;
-        }
+        TryMoveX(mvdir.x);
+        TryMoveY(mvdir.y);
+        TryMoveZ(mvdir.z);
     }
+}
 
-    int mouse_x, mouse_y;
-    QuickCG::getMouseState(mouse_x, mouse_y);
+void Game::HandleMouseMove(int mouse_x, int mouse_y) {
+    float rot_speed = frame_time_ * 0.05;
+
     SDL_WarpMouse(screen_width_ / 2, screen_height_ / 2);
     if (mouse_x != screen_width_ / 2) {
         int delta_x = mouse_x - screen_width_ / 2;
@@ -306,56 +340,143 @@ void Game::HandleInput() {
     if (mouse_y != screen_height_ / 2) {
         int delta_y = mouse_y - screen_height_ / 2;
         float angle = rot_speed * delta_y;
-        // 視点が逆立ち -> plane_y.yが-
-        if (CanRotateY(angle)) {
-            dir_ = glm::rotate(dir_, angle, plane_x_);
-            plane_y_ = glm::rotate(plane_y_, angle, plane_x_);
-        }
+        TryRotateY(angle);
     }
 }
 
-bool Game::CanRotateY(float angle) {
+void Game::OnLeftButtonPress() {
+    Ray ray;
+    bool hit = CastRay(screen_width_ / 2, screen_height_ / 2, ray);
+    if (!hit || ray.perp_wall_dist > kPlayerDestBlockDist) {
+        return;
+    }
+    SetMapBlock(ray.pos, kAirBlock);
+}
+
+void Game::OnRightButtonPress() {
+    Ray ray;
+    bool hit = CastRay(screen_width_ / 2, screen_height_ / 2, ray);
+    if (!hit || ray.perp_wall_dist > kPlayerPutBlockDist) {
+        return;
+    }
+    glm::ivec3 block_pos = ray.pos;
+    if (ray.collision_side == 0) {
+        block_pos.x += ray.dir.x < 0 ? 1 : -1;
+    }
+    if (ray.collision_side == 1) {
+        block_pos.y += ray.dir.y < 0 ? 1 : -1;
+    }
+    if (ray.collision_side == 2) {
+        block_pos.z += ray.dir.z < 0 ? 1 : -1;
+    }
+
+    assert(GetMapBlock(block_pos) == 0);
+
+    // TODO: select block
+    SetMapBlock(block_pos, 1);
+    hit = HitBlock({
+        glm::ivec3(0, 0, 0), glm::ivec3(0, 0, 1),
+        glm::ivec3(0, 1, 0), glm::ivec3(0, 1, 1),
+        glm::ivec3(0, 2, 0), glm::ivec3(0, 2, 1),
+        glm::ivec3(1, 0, 0), glm::ivec3(1, 0, 1),
+        glm::ivec3(1, 1, 0), glm::ivec3(1, 1, 1),
+        glm::ivec3(1, 2, 0), glm::ivec3(1, 2, 1),
+    });
+    if (hit) {
+        SetMapBlock(block_pos, kAirBlock);
+    }
+}
+
+void Game::HandleInput() {
+    HandleKeys();
+
+    int mouse_x, mouse_y;
+    bool lmb, rmb;
+    QuickCG::getMouseState(mouse_x, mouse_y, lmb, rmb);
+
+    HandleMouseMove(mouse_x, mouse_y);
+    if (!prev_lmb_ && lmb) {
+        OnLeftButtonPress();
+    }
+    if (!prev_rmb_ && rmb) {
+        OnRightButtonPress();
+    }
+    prev_lmb_ = lmb;
+    prev_rmb_ = rmb;
+}
+
+glm::ivec3 Game::GetPlayerPartPos(const glm::ivec3 &part) const {
+    assert(0 <= part.x && part.x <= 1 && 0 <= part.y && part.y <= 2 &&
+        0 <= part.z && part.z <= 1);
+    glm::ivec3 part_pos;
+
+    part_pos.x = pos_.x + (part.x == 0 ? -kPlayerHalfWidth : kPlayerHalfWidth);
+    if (part.y == 0) {
+        part_pos.y = pos_.y - kPlayerLowerHalfHeight;
+    }
+    else if (part.y == 1) {
+        part_pos.y = pos_.y - kPlayerLowerHalfHeight + 1.0;
+    }
+    else if (part.y == 2) {
+        part_pos.y = pos_.y + kPlayerUpperHalfHeight;
+    }
+    part_pos.z = pos_.z + (part.z == 0 ? -kPlayerHalfDepth : kPlayerHalfDepth);
+
+    return part_pos;
+}
+
+bool Game::HitBlock(const std::vector<glm::ivec3> &parts) const {
+    bool hit = false;
+    for (const glm::ivec3 &part : parts) {
+        hit |= GetMapBlock(GetPlayerPartPos(part)) != kAirBlock;
+    }
+    return hit;
+}
+
+void Game::TryRotateY(float angle) {
+    // 視点が逆立ち -> plane_y.yが-
     glm::vec3 new_plane_y = glm::rotate(plane_y_, angle, plane_x_);
-    return new_plane_y.y > 0;
+    if (new_plane_y.y > 0) {
+        dir_ = glm::rotate(dir_, angle, plane_x_);
+        plane_y_ = new_plane_y;
+    }
 }
 
-bool Game::CanMoveX(float mvx) {
-    float width = mvx > 0 ? kPlayerHalfWidth : -kPlayerHalfWidth;
-    return
-        GetMapBlock(pos_.x + width + mvx, pos_.y + kPlayerUpperHalfHeight,
-            pos_.z + kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x + width + mvx, pos_.y + kPlayerUpperHalfHeight,
-            pos_.z - kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x + width + mvx, pos_.y - kPlayerLowerHalfHeight,
-            pos_.z + kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x + width + mvx, pos_.y - kPlayerLowerHalfHeight,
-            pos_.z - kPlayerHalfDepth) == 0;
+void Game::TryMoveX(float mvx) {
+    pos_.x += mvx;
+    int part_x = mvx < 0 ? 0 : 1;
+    bool hit = HitBlock({
+        glm::ivec3(part_x, 0, 0), glm::ivec3(part_x, 0, 1),
+        glm::ivec3(part_x, 1, 0), glm::ivec3(part_x, 1, 1),
+        glm::ivec3(part_x, 2, 0), glm::ivec3(part_x, 2, 1),
+    });
+    if (hit) {
+        pos_.x -= mvx;
+    }
 }
 
-bool Game::CanMoveY(float mvy) {
-    float height = mvy > 0 ? kPlayerUpperHalfHeight : -kPlayerLowerHalfHeight;
-    return
-        GetMapBlock(pos_.x + kPlayerHalfWidth, pos_.y + height + mvy,
-            pos_.z + kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x + kPlayerHalfWidth, pos_.y + height + mvy,
-            pos_.z - kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x - kPlayerHalfWidth, pos_.y + height + mvy,
-            pos_.z + kPlayerHalfDepth) == 0 &&
-        GetMapBlock(pos_.x - kPlayerHalfWidth, pos_.y + height + mvy,
-            pos_.z - kPlayerHalfDepth) == 0;
+void Game::TryMoveY(float mvy) {
+    pos_.y += mvy;
+    int part_y = mvy < 0 ? 0 : 2;
+    bool hit = HitBlock({
+        glm::ivec3(0, part_y, 0), glm::ivec3(0, part_y, 1),
+        glm::ivec3(1, part_y, 0), glm::ivec3(1, part_y, 1),
+    });
+    if (hit) {
+        pos_.y -= mvy;
+    }
 }
 
-bool Game::CanMoveZ(float mvz) {
-    float depth = mvz > 0 ? kPlayerHalfDepth : -kPlayerHalfDepth;
-    return
-        GetMapBlock(pos_.x + kPlayerHalfWidth, pos_.y + kPlayerUpperHalfHeight,
-            pos_.z + depth + mvz) == 0 &&
-        GetMapBlock(pos_.x + kPlayerHalfWidth, pos_.y - kPlayerLowerHalfHeight,
-            pos_.z + depth + mvz) == 0 &&
-        GetMapBlock(pos_.x - kPlayerHalfWidth, pos_.y + kPlayerUpperHalfHeight,
-            pos_.z + depth + mvz) == 0 &&
-        GetMapBlock(pos_.x - kPlayerHalfWidth, pos_.y - kPlayerLowerHalfHeight,
-            pos_.z + depth + mvz) == 0;
+void Game::TryMoveZ(float mvz) {
+    pos_.z += mvz;
+    int part_z = mvz < 0 ? 0 : 1;
+    bool hit = HitBlock({
+        glm::ivec3(0, 0, part_z), glm::ivec3(0, 1, part_z), glm::ivec3(0, 2, part_z),
+        glm::ivec3(1, 0, part_z), glm::ivec3(1, 1, part_z), glm::ivec3(1, 2, part_z),
+    });
+    if (hit) {
+        pos_.z -= mvz;
+    }
 }
 
 void Game::Quit() {
